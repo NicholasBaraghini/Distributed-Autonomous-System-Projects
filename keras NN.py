2dd93c47454467d371c9805577083ec62055bea1
fedeiada <fede.iada96@gmail.com>
2022-06-30 22:16:38 +0200

    provo a mandarti roba

---
+++ keras NN.py
@@ -0,0 +1,38 @@
+from keras.datasets import mnist
+from keras.models import Sequential, load_model
+from keras.layers.core import Dense, Dropout, Activation
+from keras.utils import np_utils
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+# shape of the numpy arrays
+print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
+
+# let's print the shape before we reshape and normalize
+print("X_train shape", X_train.shape)
+print("y_train shape", y_train.shape)
+print("X_test shape", X_test.shape)
+print("y_test shape", y_test.shape)
+
+# building the input vector from the 28x28 pixels
+X_train = X_train.reshape(60000, 784)
+X_test = X_test.reshape(10000, 784)
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+
+# normalizing the data to help with the training
+X_train /= 255
+X_test /= 255
+
+# print the final input shape ready for training
+print("Train matrix shape", X_train.shape)
+print("Test matrix shape", X_test.shape)
+
+# one-hot encoding using keras' numpy-related utilities
+n_classes = 10
+print("Shape before one-hot encoding: ", y_train.shape)
+Y_train = np_utils.to_categorical(y_train, n_classes)
+Y_train[Y_train == 0] = -1      # substitute 0 with -1
+Y_test = np_utils.to_categorical(y_test, n_classes)
+Y_test[Y_test ==0] = -1         # substitute 0 with -1
+print("Shape after one-hot encoding: ", Y_train.shape)
